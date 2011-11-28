60d9b0a857edf4ccba1ee56b64dc3bc8ed39212f
wfernley <wfernley@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-28 19:58:02 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fs-real-estate-plugin/trunk@468739 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/listing_featured.php
+++
@@ -1,3 +0,0 @@
-<?php
-	echo fsrep_listings('', '', '', '', '', '', TRUE);
-?>
--- includes/listing_local.php
+++
@@ -1,4 +0,0 @@
-<?php
-	//   fsrep_listings($category_id, $value, $type, $fpagination, $hpagination, $CategoryPE)
-	echo fsrep_listings(0, $Value, $Type, 0, 0, 0, TRUE);
-?>
