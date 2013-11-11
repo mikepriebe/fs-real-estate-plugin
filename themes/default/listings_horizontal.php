5b2cfdd08647929b235806cf908b036590d267d8
wfernley <wfernley@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-11 16:41:38 +0000

    Update to 2.7.9
    git-svn-id: https://plugins.svn.wordpress.org/fs-real-estate-plugin/trunk@802542 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ themes/default/listings_horizontal.php
@@ -0,0 +1,22 @@
+<?php
+	$PageContent .= '<div id="vlistings"';
+	//if ($Listings->listing_featured == 1) {
+		//$PageContent .= ' class="featured"';
+	//} else {
+		$PageContent .= ' class="standard"';
+	//}
+	$PageContent .= ' style="width: '.$FSREPconfig['ListingsPerLine'].'%;">';
+	if (file_exists($WPUploadDir['basedir'].'/fsrep/houses/medium/'.$Listings->listing_id.'.jpg')) { 
+		$PageContent .= '<div id="listingimagev"><a href="'.fsrep_listing_url_gen($Listings->listing_id).'"'.$ListingTarget.' title="Listing"><img src="'.$WPUploadDir['baseurl'].'/fsrep/houses/medium/'.$Listings->listing_id.'.jpg" border="0" alt="'.strip_tags(fsrep_listing_name_gen($Listings->listing_id, $FSREPconfig['ListingNameDisplay'])).' - '.fsrep_get_address_name($Listings->listing_address_city, 'city').', '.fsrep_get_address_name($Listings->listing_address_province, 'province').'" style="border: 1px solid #999999;" /></a></div>';
+	}
+	$ListingTarget = ''; if (function_exists('fsrep_pro_listing_target')) { $ListingTarget = fsrep_pro_listing_target(); }
+	$PageContent .= '<h3><a href="'.fsrep_listing_url_gen($Listings->listing_id).'"'.$ListingTarget.'>'.fsrep_listing_name_gen($Listings->listing_id, $FSREPconfig['ListingNameDisplay']).'</a></h3>';
+	if ($FSREPconfig['DisplayCurrency'] == 'Yes') { $CurrencyDisplay = ' '.$FSREPconfig['CurrencyType']; } else { $CurrencyDisplay = ''; }
+	if ($Listings->listing_price_num != '0.00') {
+		$PageContent .= '<p><span class="listingprice">';
+		if ($FSREPconfig['ListingPriceID'] != '') { $PageContent .= $FSREPconfig['ListingPriceID'].' '; }
+		$PageContent .= $FSREPconfig['Currency'].fsrep_currency_format($Listings->listing_price_num).$CurrencyDisplay.'</span><br />';
+	}
+	if ($FSREPconfig['EnableCompare'] == 'Yes') { $PageContent .= 'Compare <input type="checkbox" name="'.$Listings->listing_id.'" value="'.$Listings->listing_id.'">'; }
+	$PageContent .= '</div>';
+?>
